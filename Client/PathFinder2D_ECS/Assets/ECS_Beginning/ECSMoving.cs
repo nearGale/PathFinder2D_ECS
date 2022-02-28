0e08b00f6badbe0c4d6aff10919431023db5b104
重剑无锋 <xuuold@gmail.com>
2022-03-01 00:00:21 +0800

    升级Unity版本+ECS示例

---
+++ Client/PathFinder2D_ECS/Assets/ECS_Beginning/ECSMoving.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Rendering;
+using Unity.Mathematics;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private Mesh mesh;
+    [SerializeField] private Material material;
+
+    void Start()
+    {
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof(LevelConmponent),
+            typeof(Translation),
+            typeof(RenderMesh),
+            typeof(LocalToWorld),
+            typeof(RenderBounds),
+            typeof(WorldRenderBounds),
+            typeof(ChunkWorldRenderBounds),
+            typeof(MoveSpeedComponent)
+        );
+
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(10000, Allocator.Temp);
+
+        entityManager.CreateEntity(entityArchetype, entityArray);
+
+        for(int i = 0; i < entityArray.Length; i++)
+        {
+            Entity entity = entityArray[i];
+            entityManager.SetComponentData(entity, new LevelConmponent { level = UnityEngine.Random.Range(10,20) });
+            entityManager.SetComponentData(entity, new MoveSpeedComponent { moveSpeed = UnityEngine.Random.Range(1f, 2f) });
+            entityManager.SetComponentData(entity, new Translation { Value = new float3(UnityEngine.Random.Range(-8f, 8f), UnityEngine.Random.Range(-5f, 5f), 0) });
+
+            entityManager.SetSharedComponentData(entity, new RenderMesh
+            {
+                mesh = mesh,
+                material = material,
+            });
+        }
+
+        entityArray.Dispose();
+    }
+}
---
+++ Client/PathFinder2D_ECS/Assets/ECS_Beginning/LevelUpSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class LevelUpSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref LevelConmponent levelComponent) =>
+        {
+            levelComponent.level += 1f * Time.DeltaTime;
+        });
+    }
+}
